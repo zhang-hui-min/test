ebdc31bd9b4fba88918e04f8dbd205de12e196f6
zhanghuimin <850273148@QQ.com>
2020-03-13 19:16:19 +0800

    更改主方法输出文字

--- src/main/java/com/dianpu/Application.java
+++ src/main/java/com/dianpu/Application.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
-        System.out.println("haha");
+        System.out.println("测试上传");
     }
 }
